0e30758795b6ff88af12bc88332debc8452297e5
Luca Zampetti <lzampetti@gmail.com>
2022-04-07 15:04:43 +0200

    b-here-sso-consumer

--- server/main.js
+++ server/main.js
@@ -11,14 +11,14 @@ const SingleSignOnTokenInterceptor = require('./sso/sso-token.interceptor');
 
 function serve(options) {
 
-	const PORT = process.env.PORT || 5000;
+	const PORT = 5000;
 	const PORT_HTTPS = 10443;
 
 	options = options || {};
 	options.dirname = options.dirname || path.join(__dirname, '../');
 	options.name = options.name || 'bhere-sso';
 	options.baseHref = options.baseHref || `/${options.name}/`;
-	options.port = options.port || PORT;
+	options.port = process.env.PORT || options.port || PORT;
 	options.portHttps = options.portHttps || PORT_HTTPS;
 	console.log('serve', options);
 
